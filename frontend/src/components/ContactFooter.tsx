4544cc4bc6b4a789a5b74f3f9e8c856732ce2d43
Jack Criminger <jack.crim828@gmail.com>
2026-04-18 16:45:00 -0700

    footer working except email issues

--- frontend/src/components/ContactFooter.tsx
+++ frontend/src/components/ContactFooter.tsx
@@ -1,9 +1,29 @@
 import './ContactFooter.css'
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa'
 
 const ContactFooter = () => {
+    const links = [
+        { icon: FaGithub, url: 'https://github.com/JCrim828', label: 'GitHub' },
+        { icon: FaLinkedin, url: 'https://www.linkedin.com/in/jack-criminger/', label: 'LinkedIn'},
+        { icon: FaEnvelope, url: 'mailto:jack.crim828@gmail.com', label: 'Email'},
+    ]
+
     return (
         <div className='footer'>
-            *Contact Information Icons Here*
+            <div className='social-links'>
+                {links.map((link) => (
+                    <a
+                        key={link.label}
+                        href={link.url}
+                        target={link.url.startsWith('mailto') ? undefined : '_blank'}
+                        rel={link.url.startsWith('mailto') ? undefined : 'noopener noreferrer'}
+                        aria-label={link.label}
+                        className='social-icon'
+                    >
+                        <link.icon />
+                    </a>
+                ))}
+            </div>
         </div>
     )
 }
